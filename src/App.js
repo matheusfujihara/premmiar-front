6e80de6beff3b12e41b2aa3633f0c8cacb0187c2
Matheus Fujihara <fujihara_matheus@hotmail.com>
2021-02-22 14:35:40 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,102 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from "react";
 import './App.css';
+import Axios from 'axios';
+
 
 function App() {
+
+  const [ name, setName ] = useState('');
+  const [ username, setUsername ] = useState('');
+  const [ password, setPassword ] = useState('');
+  const [ userList, setUserList ] = useState([]);
+  const [ newName, setNewName ] = useState('');
+  const [ newEmail, setNewEmail ] = useState('');
+
+  useEffect(() => {
+    Axios
+      .get("http://localhost:8080/user")
+      .then((response) => {
+        setUserList(response.data.users);
+      });
+  }, []);
+
+  const submitUser = () => {
+    Axios.post("http://localhost:8080/user", {
+      name: name,
+      email: username,
+      password: password
+    });
+
+    setUserList([
+      ...userList,
+      { name: name, email: username }
+    ]);
+  };
+
+  const deleteUser = (userEmail) => {
+    Axios.delete(`http://localhost:8080/user/${userEmail}`)
+  }
+
+  const updateUser = (userEmail) => {
+    Axios.put(`http://localhost:8080/user/${userEmail}`, {
+      name: newName,
+      email: newEmail
+    });
+    setNewName("");
+    setNewEmail("");
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>CRUD User</h1>
+    
+      <div className="form">
+
+        <label>Name:</label>
+        <input type="text" name="name" onChange={(e) => {
+          setName(e.target.value)
+        }} />
+
+        <label>Username:</label>
+        <input type="text" name="username" onChange={(e) => {
+          setUsername(e.target.value)
+        }} />
+
+        <label>Password:</label>
+        <input type="text" name="password" onChange={(e) => {
+          setPassword(e.target.value)
+        }} />
+
+        <div className="button-div">
+          <button className="button" onClick={ submitUser }>Submit</button>
+        </div>
+
+        { userList.map((item) => {
+          return (
+            <div className="card-user">
+              <h1>{ item.name }</h1>
+              <p>{ item.email }</p>
+
+              <button onClick={() => {
+                deleteUser(item.email)
+              }}>Delete</button>
+
+              <input type="text" id="update-insert" onChange={(e) => {
+                setNewName(e.target.value)
+              }} placeholder="New Name..." />
+
+              <input type="text" id="update-insert" onChange={(e) => {
+                setNewEmail(e.target.value)
+              }} placeholder="New Email..." />
+              <button onClick={() => {updateUser(item.email)}}>Update</button>
+            </div>
+          )
+        })}
+
+      </div>
+
     </div>
+    
   );
 }
 
